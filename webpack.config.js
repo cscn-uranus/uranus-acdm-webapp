e4e5f92bb290cd58c54dd5fac7a915c70878a750
tellxp <xiao.peng@live.cn>
2017-08-07 17:53:52 +0800

    kendoui含有css文件，直接使用css文件载入，加入file-loader来载入font的规则

--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.txt$/,
-        use: 'raw-loader',
-      },
-      {
         test: /\.html$/,
         use: [
           {
@@ -49,18 +45,23 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-
-      {
         test: /\.scss$/,
         use: [
           'style-loader',
           'css-loader',
           'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
         ],
       },
     ],
